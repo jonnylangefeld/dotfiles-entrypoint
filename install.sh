62c855c7aa1d166b07343f7eb8e3363355dedd59
Jonny langefeld <jonnylangefeld@gmail.com>
2024-09-21 18:46:36 -0700

    improve entrypoint

--- install.sh
+++ install.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
 
-if [ ! -f ~/.config/op/token ]; then
-  echo "paste the 1Password service account token from here: https://my.1password.com/vaults/nkt3i4w35tbebpteodehlxamey/allitems/pt36wyw4hk653cz7xqqkbeioji:"
-  read -r OP_SERVICE_ACCOUNT_TOKEN
-  mkdir -p ~/.config/op
-  echo "${OP_SERVICE_ACCOUNT_TOKEN}" > ~/.config/op/token
-fi
 export OP_SERVICE_ACCOUNT_TOKEN
-OP_SERVICE_ACCOUNT_TOKEN=$(cat ~/.config/op/token)
+echo "paste the 1Password service account token from here: https://my.1password.com/vaults/nkt3i4w35tbebpteodehlxamey/allitems/pt36wyw4hk653cz7xqqkbeioji:"
+read -r OP_SERVICE_ACCOUNT_TOKEN
 
 # passwordless sudo
-echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$USER
+echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/"$USER"
 
 # install homebrew
 if ! brew >/dev/null 2>&1 ; then
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # install 1password
 brew install --cask 1password-cli
@@ -23,9 +20,15 @@ brew install --cask 1password-cli
 mkdir -p ~/.ssh
 op read -f -o ~/.ssh/github "op://Service Account/github/private key"
 
-echo "github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl" > ~/.ssh/known_hosts_tmp
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/github
+chmod 644 ~/.ssh/github.pub
 
-export GIT_SSH_COMMAND='ssh -i ~/.ssh/github -o UserKnownHostsFile=~/.ssh/known_hosts_tmp'
+echo "github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl" >> ~/.ssh/known_hosts
+
+export GIT_SSH_COMMAND='ssh -i ~/.ssh/github -o UserKnownHostsFile=~/.ssh/known_hosts'
 
 mkdir -p ~/repos
 git clone git@github.com:jonnylangefeld/dotfiles.git ~/repos/dotfiles
+
+cd ~/repos/dotfiles || exit
