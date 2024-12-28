37908b71cb658a1812bf70304ecfb30770bd947b
Jonny langefeld <jonny.langefeld@snowflake.com>
2024-12-28 12:57:34 -0800

    test

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ Run
 
 ```shell
 sh -c "$(curl -fsLS https://raw.githubusercontent.com/jonnylangefeld/dotfiles-entrypoint/jlf/universal/install.sh)"
+
+sh -c "$(curl -H "Accept: application/vnd.github.v3.raw" \
+     https://api.github.com/repos/jonnylangefeld/dotfiles-entrypoint/contents/install.sh\?ref=jlf/universal)"
 ```
 
 On any machine to install the dotfiles.
--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/"$USER"
 
 if ! command -v nix &> /dev/null; then
-  /bin/bash -c "$(curl -fsSL https://nixos.org/nix/install)" --yes
+  sh -c "$(curl -fsSL https://nixos.org/nix/install)" --yes
   # /bin/bash <(curl -L https://nixos.org/nix/install) --yes
   . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 fi
