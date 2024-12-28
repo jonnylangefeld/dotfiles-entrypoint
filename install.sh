cba73064a3b980e0c3ec6a50f6fbb11144109fa9
Jonny langefeld <jonny.langefeld@snowflake.com>
2024-12-28 14:09:16 -0800

    test

--- install.sh
+++ install.sh
@@ -12,11 +12,7 @@ if ! command -v nix &> /dev/null; then
 fi
 
 echo "running nix-shell"
-
-# because otherwise I'd run into https://github.com/NixOS/docker/issues/34
-$SHELL
-
-# shellcheck disable=SC2016
+exec $SHELL <<EOF
 nix-shell -p google-cloud-sdk git --run '
   current_user=$(gcloud auth list --filter=status:ACTIVE --format="value(account)")
   if [ "$current_user" != "jonny.langefeld@gmail.com" ]; then
@@ -34,3 +30,26 @@ nix-shell -p google-cloud-sdk git --run '
     cd ~/repos/dotfiles || exit
   fi
 '
+EOF
+
+# because otherwise I'd run into https://github.com/NixOS/docker/issues/34
+# exec $SHELL
+
+# # shellcheck disable=SC2016
+# nix-shell -p google-cloud-sdk git --run '
+#   current_user=$(gcloud auth list --filter=status:ACTIVE --format="value(account)")
+#   if [ "$current_user" != "jonny.langefeld@gmail.com" ]; then
+#     gcloud auth login --no-launch-browser
+#   fi
+
+#   gh_token=$(gcloud secrets versions access latest --secret="gh_token" --project jonnylangefeld-dotfiles)
+
+#   mkdir -p ~/repos
+#   if [ -d ~/repos/dotfiles ]; then
+#     cd ~/repos/dotfiles || exit
+#     git pull
+#   else
+#     git clone --depth 1 "https://$gh_token@github.com/jonnylangefeld/dotfiles.git" ~/repos/dotfiles
+#     cd ~/repos/dotfiles || exit
+#   fi
+# '
