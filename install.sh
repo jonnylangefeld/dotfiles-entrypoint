acdc27c4d4457f78902dea182e854820fad7e5c4
Jonny langefeld <jonny.langefeld@snowflake.com>
2024-12-28 12:38:41 -0800

    test

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/"$USER"
 
 if ! command -v nix &> /dev/null; then
-  sh <(curl -L https://nixos.org/nix/install) --yes
+  /bin/bash <(curl -L https://nixos.org/nix/install) --yes
   . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 fi
 
