65e66d932d70ca37cac7896744986214d45032c7
Jonny langefeld <jonny.langefeld@snowflake.com>
2024-12-28 12:33:08 -0800

    test

--- install.sh
+++ install.sh
@@ -1,39 +1,28 @@
 #!/bin/sh
 
 # passwordless sudo
-echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/"$USER"
+# echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/"$USER"
 
-# install homebrew
-if ! brew >/dev/null 2>&1 ; then
-  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! command -v nix &> /dev/null; then
+  sh <(curl -L https://nixos.org/nix/install) --yes
+  . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 fi
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# shellcheck disable=SC2016
+nix-shell -p google-cloud-sdk git --run '
+  current_user=$(gcloud auth list --filter=status:ACTIVE --format="value(account)")
+  if [ "$current_user" != "jonny.langefeld@gmail.com" ]; then
+    gcloud auth login --no-launch-browser
+  fi
 
-# install homebrew packages
-xargs brew install --cask << EOF
-google-cloud-sdk
-EOF
+  gh_token=$(gcloud secrets versions access latest --secret="gh_token" --project jonnylangefeld-dotfiles)
 
-# login to gcloud
-current_user=$(gcloud auth list --filter=status:ACTIVE --format="value(account)")
-if [ "$current_user" != "jonny.langefeld@gmail.com" ]; then
-  gcloud auth login --no-launch-browser
-fi
-
-# create secret via
-# gcloud secrets create gh_token --project jonnylangefeld-dotfiles
-# update secret via
-# echo -n "$gh_token" | gcloud secrets versions add gh_token --data-file=- --project jonnylangefeld-dotfiles
-gh_token=$(gcloud secrets versions access latest --secret="gh_token" --project jonnylangefeld-dotfiles)
-
-# clone dotfiles
-mkdir -p ~/repos
-if [ -d ~/repos/dotfiles ]; then
-  cd ~/repos/dotfiles || exit
-  git pull
-else
-  git clone --depth 1 "https://$gh_token@github.com/jonnylangefeld/dotfiles.git" ~/repos/dotfiles
-  cd ~/repos/dotfiles || exit
-  ls
-fi
+  mkdir -p ~/repos
+  if [ -d ~/repos/dotfiles ]; then
+    cd ~/repos/dotfiles || exit
+    git pull
+  else
+    git clone --depth 1 "https://$gh_token@github.com/jonnylangefeld/dotfiles.git" ~/repos/dotfiles
+    cd ~/repos/dotfiles || exit
+  fi
+'
