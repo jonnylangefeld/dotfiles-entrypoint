aa36442d5c5ea15882966e42daeb5af38e81f137
Jonny langefeld <jonny.langefeld@snowflake.com>
2024-12-28 13:00:07 -0800

    test

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@
 # echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/"$USER"
 
 if ! command -v nix &> /dev/null; then
-  sh -c "$(curl -fsSL https://nixos.org/nix/install)" --yes
+  curl -L https://nixos.org/nix/install | sh -c 'sh --yes'
+  # sh -c "$(curl -fsSL https://nixos.org/nix/install)" --yes
   # /bin/bash <(curl -L https://nixos.org/nix/install) --yes
   . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 fi
