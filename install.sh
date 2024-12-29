a84146cfdc3583584080e3579ebde379d6a6174b
Jonny langefeld <jonny.langefeld@snowflake.com>
2024-12-29 09:07:34 -0800

    test

--- install.sh
+++ install.sh
@@ -22,6 +22,9 @@ install_developer_tools() {
 if ! command -v nix >/dev/null 2>&1; then
   curl -L https://nixos.org/nix/install | sh -s -- --yes
   . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+
+
+  curl -L https://nixos.org/nix/install | sh -s -- --yes
 fi
 
 echo "running nix-shell"
