86cfadbd03e5eb927cbc390bfc80c18e1864642d
Jonny langefeld <jonny.langefeld@snowflake.com>
2024-12-28 16:32:16 -0800

    test

--- install.sh
+++ install.sh
@@ -15,7 +15,9 @@ echo "running nix-shell"
 
 # because otherwise I'd run into https://github.com/NixOS/docker/issues/34
 # exec $SHELL
-sudo chown -R ${USER}:staff /nix/var/nix/profiles/per-user
+# sudo chown -R ${USER}:staff /nix/var/nix/profiles/per-user
+# sudo chown -R ${USER}:staff /nix/var/nix/gcroots/per-user
+sudo chown -R ${USER}:staff /nix
 
 # shellcheck disable=SC2016
 nix-shell -p google-cloud-sdk git --run '
